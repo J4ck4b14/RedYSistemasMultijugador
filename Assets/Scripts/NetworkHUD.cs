c9d76f94d2bc26b28f8e90a6dcc735440409ff5c
alexrica <alejandrorica00@gmail.com>
2025-03-21 20:47:28 +0100

    CambiosPrevios_a_RPC
    Push a tope

--- Assets/Scripts/NetworkHUD.cs
+++ Assets/Scripts/NetworkHUD.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Netcode;
+
 public class NetworkHUD : MonoBehaviour
 {
     void OnGUI()
@@ -7,8 +8,7 @@ public class NetworkHUD : MonoBehaviour
         // Creamos una interfaz en pantalla
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
         // Si no somo un cliente o un servidor
-        if (!NetworkManager.Singleton.IsClient && !
-        NetworkManager.Singleton.IsServer)
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
             // Mostramos los botones para iniciar una conexion
             StartButtons();
@@ -19,9 +19,40 @@ public class NetworkHUD : MonoBehaviour
             StatusLabels();
             // Si queremos pedir una nueva posicion
             SubmitNewPosition();
+            // Si soy el servidor, mostrar boton
+            if(NetworkManager.Singleton.IsServer)
+            {
+                if (GUILayout.Button("Quitar vida")) // Muestra un boton con el texto azul
+                {
+                    // Se ejecuta cuando se pulsa el boton
+                    var players = GameObject.FindObjectsOfType<Player>(); // Devuelve un array con los componentes
+                    var playersGameObjects = GameObject.FindGameObjectsWithTag("Player"); // Devuelve un array con los GameObjects
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        var player = players[i];
+                        int cantidad = Random.Range(10, 100);
+                        // 5º El servidor hace el cambio en la NetworkVariable
+                        player.QuitarVida(cantidad);
+                    }
+                }
+                if (GUILayout.Button("Cambiar el color"))
+                {
+                    var players1 = GameObject.FindObjectsOfType<Player>(); // Devuelve un array con los componentes
+                    var playersGameObjects1 = GameObject.FindGameObjectsWithTag("Player"); // Devuelve un array con los GameObjects
+                    for (int i = 0; i < players1.Length; i++)
+                    {
+                        var player = players1[i];
+                        Color color = new Color(Random.value, Random.value, Random.value);
+                        player.CambiarColor(color);
+                    }
+                }
+            }
+            // Si soy el cliente, mostrar boton de cambio de color al servidor
         }
+
         GUILayout.EndArea();
     }
+
     // Cada boton inicia un tipo de servicio en el ordenador
     static void StartButtons()
     {
@@ -29,20 +60,22 @@ public class NetworkHUD : MonoBehaviour
         if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
         if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
     }
+
     // Muestra el estado del servicio multijugador
     static void StatusLabels()
     {
         var mode = NetworkManager.Singleton.IsHost ?
-        "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
         GUILayout.Label("Transport: " +
-        NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
         GUILayout.Label("Mode: " + mode);
     }
+
     // Peticion de movimiento para el jugador
     static void SubmitNewPosition()
     {
-        if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request" +
-            "Position Change"))
+        if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
         {
             var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
             var player = playerObject.GetComponent<Player>();
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,67 @@ public class Player : NetworkBehaviour
 {
     // Informacion que queremos sincronizar
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    // 1º Variable que sincronnizar
+    public NetworkVariable<int> NetworkSalud = new NetworkVariable<int>();
+    //Variable tipo color Sincronizar
+    public NetworkVariable<Color> NetworkColor = new NetworkVariable<Color>();
+    private TMPro.TMP_Text textoVida;
 
     // Evento que se lanza cuando se hace spawn de un objeto
     // Hay que pensar que se lanza en cada instancia de cada cliente/servidor
+    //Ejercicio: Leer la nertwork variable de color, coger su valor y aplicarlo
+    //Objetivo: Cuando el cliente se conecta, tiene que ver el color de los otros clientes que ya estaban cambiados
     public override void OnNetworkSpawn()
     { 
         // Comprobamos si somos el propietario
         if (IsOwner)
         {
             Debug.Log(IsLocalPlayer);
-            Move(); 
+            Move();
+
+            // Busco la interfaz de vida
+            var vidaPlayer = GameObject.Find("VidaPlayer");
+            textoVida = vidaPlayer.GetComponent<TMPro.TMP_Text>();
+            textoVida.text = NetworkSalud.Value.ToString();
+
+            // 4º Suscribir callback si soy el propietario
+            NetworkSalud.OnValueChanged += CambioVida;
+            GetComponent<MeshRenderer>().material.color = NetworkColor.Value;
         }
+        if (!IsServer)
+        {
+            NetworkColor.OnValueChanged += CambioColor;
+        }
+    }
+
+    // 3º Metodo callback cuando cambia la vida
+    public void CambioVida(int vidaAnterior, int nuevaVida)
+    {
+        Debug.Log("Vida went from " + vidaAnterior + " to " + nuevaVida);
+        // Tiene que hacer el cambio que ve el cliente
+        textoVida.text = nuevaVida.ToString();
+    }
+
+    public void CambioColor(Color colorAnterior, Color colorNuevo)
+    {
+        Debug.Log("Color went from " + colorAnterior + " to " + colorNuevo);
+        // Tiene que hacer el cambio que ve el cliente
+        CambiarColor(colorNuevo);
+    }
+
+    // 2º Un metodo que cambie la variable
+    public void QuitarVida(int cantidad)
+    {
+        if(IsServer)
+            NetworkSalud.Value -= cantidad; // 6º Si esto lo hace el servidor, llama al callback en los clientes
+    }
+
+    public void CambiarColor(Color color)
+    {
+        if(IsServer)
+            NetworkColor.Value = color;
+            GetComponent<MeshRenderer>().material.color = color;
+
     }
 
     // Funcion que mueve al jugador, solo puede ser ejecutada por el servidor
@@ -28,20 +78,30 @@ public class Player : NetworkBehaviour
             var randomPosition = GetRandomPositionOnPlane();
             transform.position = randomPosition;
             Position.Value = randomPosition;
+            ChangeColorRequestClientRpc();
         }
         // Si no somos el servidor, podemos pedir a este que haga el movimiento
         else
         {
-            SubmitPositionRequestServerRpc();
+            if(IsClient)
+                SubmitPositionRequestServerRpc();
         }
     }
 
     [ServerRpc] // Solo el servidor puede ejecutar estos metodos
     void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
     {
-        Position.Value = GetRandomPositionOnPlane();
+        if(IsServer)
+            Position.Value = GetRandomPositionOnPlane();
     }
+    [ClientRpc]
+    void ChangeColorRequestClientRpc()
+    {
+        if (IsClient)
+        {
 
+        }
+    }
     // Metodo que devuelve una posicion aleatoria dentro de un rango en el plano XZ
     static Vector3 GetRandomPositionOnPlane()
     {
